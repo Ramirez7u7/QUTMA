435d7a79cc59a8e31f3aa74367b78f6b46dbeb0d
Ramirez7u7 <utm23090438@utma.edu.mx>
2024-11-26 14:23:55 -0600

    cambios de usar

--- api/src/Constrollers/UserController.ts
+++ api/src/Constrollers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { UserModel } from "../models/UsersModel";
+import jwt from "jsonwebtoken";
 
 
 
@@ -28,18 +29,27 @@ export const registerUsers =async (req:Request,res:Response):Promise<any>=>{
                 msg:"no puedes crear un admin si no lo eres tambien"
             })
 
-            await UserModel.create({
+           const user = await UserModel.create({
                 name:name,
                 lastNames:lastNames,
                 email:email,
                 password:password,
                 rol:rol
             })
-            return res.status(200).json({msg:"usuario cargado con exito"})
+            const token = jwt.sign(JSON.stringify(user),"pocoyo");
+            return res.status(200).json({msg:"usuario cargado con exito",token})
 
     } catch (error) {
         console.log(error);
         return res.status(500).json({msg:"hubo en error al crear el usuario"})
         
     }
+}
+
+export const singin = async (req:Request, res:Response): Promise<void>=>{
+// carreo y contraseña
+    //verrificar que el urusairo existe
+    const user = await UserModel.findOne({correo:req.body.email,password:req.body.password});
+    //si no existe devuelven error
+    //si existe devuelven un token
 }
